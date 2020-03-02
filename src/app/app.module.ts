d5e9505e0fa135e1327a88c57a784b9eae014a9a
KeithRitter <ritter.keithpersonal@gmail.com>
2020-03-01 23:10:50 -0500

    Setup routing. (Please send, my UPS went off)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule } from '@angular/router';
+import { appRoutes } from '../routes';
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { SectionHealthComponent } from './sections/section-health/section-health.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    SectionHealthComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { SectionHealthComponent } from './app/sections/section-health/section-health.component';
+
+export const appRoutes : Routes = [
+    {path: 'health', component: SectionHealthComponent},
+];
