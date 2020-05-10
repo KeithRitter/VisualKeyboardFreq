3ae3b4e9bce966565752d2f61f5927be084e5696
KeithRitter <ritter.keithpersonal@gmail.com>
2020-05-10 03:11:46 -0400

    List page populated

--- src/app/Sections/section-list/section-list.component.ts
+++ src/app/Sections/section-list/section-list.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ModuleWithComponentFactories } from '@angular/core';
 import { InData } from '../../shared/InData';
+import { InputDataService } from '../../../input-data.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-section-list',
@@ -8,18 +10,24 @@ import { InData } from '../../shared/InData';
 })
 export class SectionListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _inputDataService: InputDataService) { }
 
-  data: InData[] = [    
-    {time: new Date(2020, 3, 3) , count: 1},
-    {time: new Date(2020, 3, 4) , count: 1},
-    {time: new Date(2020, 3, 5) , count: 1},
-    {time: new Date(2020, 3, 6) , count: 1},
-    {time: new Date(2020, 3, 7) , count: 1},
-    {time: new Date(2020, 3, 8) , count: 1}
-  ];
+  dataRes: any;
+
+ // data: InData[] = [    
+ //   {time: new Date(2020, 3, 3) , count: 1},
+ //   {time: new Date(2020, 3, 4) , count: 1},
+ //   {time: new Date(2020, 3, 5) , count: 1},
+ //   {time: new Date(2020, 3, 6) , count: 1},
+ //   {time: new Date(2020, 3, 7) , count: 1},
+  //  {time: new Date(2020, 3, 8) , count: 1}
+  //];
+  data: res;
   
   ngOnInit(): void {
+    this._inputDataService.getCount()
+    .subscribe(res => {
+      this.data = res;
+    })
   }
-
 }
